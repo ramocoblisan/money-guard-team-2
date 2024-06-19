98b38030b9db295d791e9772f15b79cccd2d9c10
czke33 <czke33@gmail.com>
2024-06-19 15:44:50 +0300

    update login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import LoginRegisterForm from '../../components/LoginRegisterForm/LoginRegisterForm';
+
+import { loginThunk } from '../../redux/auth/operations';
+import { loginSchema } from '../../Schema/loginShema';
+
+const Login = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = data => {
+    dispatch(loginThunk(data));
+  };
+
+  return (
+    <>
+      <LoginRegisterForm
+        formType={'login'}
+        onDataSubmit={handleSubmit}
+        schema={loginSchema}
+      />
+    </>
+  );
+};
+
+export default Login;
