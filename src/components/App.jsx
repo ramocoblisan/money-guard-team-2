d848d4103f8d6743c9d63ac9ec07fad57bf47c05
czke33 <czke33@gmail.com>
2024-06-19 11:29:41 +0300

    add inputfield component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,12 @@
+import React, { Suspense } from'react';
+import {Navigate, Route, Routes} from "react-router-dom";
+import {lazy} from react;
+import Loader from './Loader/Loader';
 
+function App (){
+    return (
+       <>
+       <Suspense fallback={<Loader/>}></Suspense>
+       </>
+    )
+}
