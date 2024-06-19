3141b7fe61b4d384fa4a7805ea645635e6bb1d38
ramocoblisan <ramonaioana67@yahoo.com>
2024-06-19 11:04:23 +0300

    index.js updated

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./partials/Components/App";
+import { BrowserRouter } from "react-router-dom";
+
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    {/* <BrowserRouter basename="money-guard-team3"> */}
+      <App />
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
+);
