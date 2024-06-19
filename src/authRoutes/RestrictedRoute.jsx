14bceadea14248bc649e65b1f8c06c93ebeffef7
czke33 <czke33@gmail.com>
2024-06-19 16:22:09 +0300

    add private and restricted routes

---
+++ src/authRoutes/RestrictedRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+import { selectIsLoggedIn } from '../redux/auth/authSlice';
+
+const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  const isAuth = useSelector(selectIsLoggedIn);
+
+  return isAuth ? <Navigate to={redirectTo} /> : <Component />;
+};
+
+export default RestrictedRoute;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from'react';
 import {Navigate, Route, Routes} from "react-router-dom";
-import {lazy} from react;
+
 import Loader from './Loader/Loader';
 
 function App (){
