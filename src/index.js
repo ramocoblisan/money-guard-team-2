d790c0d6cb3fe7ab1110990859bb308eb2aaf079
ramocoblisan <ramonaioana67@yahoo.com>
2024-06-20 23:06:12 +0300

    Index.js updated

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "././components/App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const rootElement = document.getElementById('root');
 
@@ -9,8 +11,10 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="money-guard-team3"> */}
-      <App />
-    {/* </BrowserRouter> */}
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
