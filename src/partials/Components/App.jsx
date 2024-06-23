679b2ab90ddabe8df1a8e695be05893dc5f7ee8e
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-06-23 19:27:15 +0300

    rebuild pages

--- src/partials/Components/App.jsx
+++ src/partials/Components/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter, HashRouter } from 'react-router-dom';
-import { RestrictedRoute } from '../../authRoutes/RestrictedRoute';
 
 import BackupHome from '../Pages/BackupHome';
 import Register from '../Pages/RegisterPage';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<BackupHome />} />
         <Route path="home" element={<BackupHome />} />
         <Route path="register" element={<Register />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
