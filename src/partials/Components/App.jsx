f4b36eaa19f55527aecc8bb2f9ef2543defd5783
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-06-23 19:36:18 +0300

    rebuild pages

--- src/partials/Components/App.jsx
+++ src/partials/Components/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter, HashRouter } from 'react-router-dom';
+import { RestrictedRoute } from '../../authRoutes/RestrictedRoute';
 
 import BackupHome from '../Pages/BackupHome';
 import Register from '../Pages/RegisterPage';
 
 function App() {
   return (
-    <BrowserRouter basename="/money-guard-team-2">
+    <HashRouter basename="/money-guard-team-2">
       <Routes>
         <Route path="/" element={<BackupHome />} />
-        <Route path="home" element={<BackupHome />} />
-        <Route path="register" element={<Register />} />
+        <Route path="/home" element={<BackupHome />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
