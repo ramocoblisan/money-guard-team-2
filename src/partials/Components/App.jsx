91eb148d5188ddaec3a852cd5d203f87e812ea45
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-06-23 19:01:56 +0300

    rebuild pages

--- src/partials/Components/App.jsx
+++ src/partials/Components/App.jsx
@@ -8,11 +8,9 @@ import Register from '../Pages/RegisterPage';
 function App() {
   return (
     <BrowserRouter basename="/money-guard-team-2">
-      <Routes>
-        <Route path="/" element={<BackupHome />} />
-        <Route path="/home" element={<BackupHome />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Route path="/" element={<BackupHome />} />
+      <Route path="/home" element={<BackupHome />} />
+      <Route path="/register" element={<Register />} />
     </BrowserRouter>
   );
 }
