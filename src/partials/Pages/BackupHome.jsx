64c0af0c1944a571cd1500517593705921adbeb7
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-06-23 20:43:52 +0300

    rebuild pages

--- src/partials/Pages/BackupHome.jsx
+++ src/partials/Pages/BackupHome.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <div>
+      <button onClick={() => navigate('/register')}></button>
       <h1>Home Page</h1>
       <p>Welcome to the Home Page!</p>
     </div>
