c7c7abf91bdff6faec87f64c437bfd81e8bef611
Victor Prisacariu <prisacariuvictor@gmail.com>
2024-06-23 16:00:47 +0300

    rebuild pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './partials/components/App';
+import App from './partials/Components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
