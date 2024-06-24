caf92c6dc17656b9a1fdef42c24cffd08a3c19fe
ramocoblisan <ramonaioana67@yahoo.com>
2024-06-24 15:00:30 +0300

    App component updated

--- src/partials/Components/App.jsx
+++ src/partials/Components/App.jsx
@@ -5,23 +5,43 @@ import { RestrictedRoute } from '../../authRoutes/RestrictedRoute';
 import BackupHome from '../Pages/BackupHome';
 import Register from '../Pages/RegisterPage';
 import Login from '../Pages/LoginPage';
+import Dashboard from '../Pages/DashboardPage';
 
-
+const basename = process.env.NODE_ENV === 'production' ? '/money-guard-team-2' : '/';
 
 function App() {
   return (
-    <BrowserRouter basename="money-guard-team-2">
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<BackupHome />} />
         <Route path="/home" element={<BackupHome />} />
-        <Route
+        {/* <Route
           path="/register"
-          element={<RestrictedRoute component={<Register />} redirectTo="/" />}
-        />
+          element={<RestrictedRoute component={Register} redirectTo="/" />}
+        /> */}
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route 
+          path="/dashboard" 
+          element={<RestrictedRoute component={Dashboard} redirectTo="/login" />}
+        />
       </Routes>
     </BrowserRouter>
   );
 }
+
+// function App() {
+//   return (
+//     <BrowserRouter basename={basename}>
+//       <Routes>
+//         <Route path="/" element={<BackupHome />} />
+//         <Route path="/home" element={<BackupHome />} />
+//         <Route path="/register" element={<RestrictedRoute component={Register} redirectTo="/" />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/dashboard" element={<RestrictedRoute component={Dashboard} redirectTo="/login" />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
 export default App;
