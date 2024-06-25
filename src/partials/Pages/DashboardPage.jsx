c3090ca82fcc3d6e65a37e3150f9b9bd5d2867ae
ramocoblisan <ramonaioana67@yahoo.com>
2024-06-25 23:17:43 +0300

    DashboardPage background added

--- src/partials/Pages/DashboardPage.jsx
+++ src/partials/Pages/DashboardPage.jsx
@@ -6,28 +6,31 @@ import Statistics from './StatisticsPage';
 import Balance from '../Components/Balance';
 import Currency from '../Components/Currency';
 import { Link } from 'react-router-dom';
+import * as style from '../../sass/Module/DashboardPage.module.scss';
 
 const Dashboard = () => {
   return (
     <div>
       <Header />
-      <div style={{ display: 'flex' , margin: '20px' }}>
-        <nav style={{ marginRight: '100px' }}>
-          <ul>
-            <li><Link to="/dashboard/home">Home</Link></li>
-            <li><Link to="/dashboard/statistics">Statistics</Link></li>
-          </ul>
-        </nav>
-        <div style={{ flexGrow: 1 }}>
-          <Routes>
-            <Route path="home" element={<Home />} />
-            <Route path="statistics" element={<Statistics />} />
-            <Route path="/" element={<Navigate to="home" />} />
-          </Routes>
+      <div className={style.dashboardContent}>
+        <div style={{ display: 'flex' , margin: '20px' }}>
+          <nav style={{ marginRight: '100px' }}>
+            <ul>
+              <li><Link to="/dashboard/home">Home</Link></li>
+              <li><Link to="/dashboard/statistics">Statistics</Link></li>
+            </ul>
+          </nav>
+          <div style={{ flexGrow: 1 }}>
+            <Routes>
+              <Route path="home" element={<Home />} />
+              <Route path="statistics" element={<Statistics />} />
+              <Route path="/" element={<Navigate to="home" />} />
+            </Routes>
+          </div>
         </div>
+        <Balance />
+        <Currency />
       </div>
-      <Balance />
-      <Currency />
     </div>
   );
 };
