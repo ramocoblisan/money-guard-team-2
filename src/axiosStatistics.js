6abfdc9351ab860016cbdf9cf73c37457f45187d
ramocoblisan <ramonaioana67@yahoo.com>
2024-06-30 18:49:33 +0300

    Statistics API Error resolved

--- src/axiosStatistics.js
+++ src/axiosStatistics.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
 
 const axiosStatistics = axios.create({
-  baseURL: 'https://moneyguardbackend.onrender.com/',
+  baseURL: 'https://wallet.b.goit.study/api',
+});
+
+// Adaugă un interceptor pentru a include token-ul în header
+axiosStatistics.interceptors.request.use(config => {
+  const token = localStorage.getItem('token'); // Sau de unde stochezi token-ul
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
 });
 
 export default axiosStatistics;
--- src/redux/statistics/statisticsOperations.js
+++ src/redux/statistics/statisticsOperations.js
@@ -1,12 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axiosStatistics from '../../axiosStatistics';
 
-
 export const getStatistics = createAsyncThunk(
   'statistics/getStatistics',
   async (query, { rejectWithValue }) => {
     try {
-      const response = await axiosStatistics.get(`/transactions/statistics?${query}`);
+      const response = await axiosStatistics.get(`/transactions-summary?${query}`);
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -18,7 +17,7 @@ export const getCategories = createAsyncThunk(
   'statistics/getCategories',
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axiosStatistics.get('/transactions/categories');
+      const response = await axiosStatistics.get('/transaction-categories');
       return response.data;
     } catch (error) {
       return rejectWithValue(error.message);
