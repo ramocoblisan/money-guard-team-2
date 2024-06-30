d179fd2117becf60c0f76d4f1aab9187f6289486
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-06-30 03:49:25 +0100

    Commit work Andrei-Gabriel Paiu-Rotundu

--- src/partials/Components/Currency.jsx
+++ src/partials/Components/Currency.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import styles from '../../sass/Module/Currency.module.css';
 
 const Currency = () => {
   return (
-    <div>
-      <h2>Currency Information</h2>
+    <div className={styles.currencyContainer}>
+      <table>
+        <tr className={styles.currencyTableHead}>
+          <th>Currency</th>
+          <th>Purchase</th>
+          <th>Sale</th>
+        </tr>
+        <tr className={styles.currencyTableBody}>
+          <td>USD</td>
+          <td>40.35</td>
+          <td>40.80</td>
+        </tr>
+        <tr className={styles.currencyTableBody}>
+          <td>EUR</td>
+          <td>43.20</td>
+          <td>43.80</td>
+        </tr>
+      </table>
     </div>
   );
 };
