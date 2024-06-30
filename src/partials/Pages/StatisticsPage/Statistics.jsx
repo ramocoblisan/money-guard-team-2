f6deed1a5ba7c7146e71716bf38842abca9c83aa
damian-ionut <143095557+damian-ionut@users.noreply.github.com>
2024-06-30 12:04:31 +0300

    update-statistics

--- src/partials/Pages/StatisticsPage/Statistics.jsx
+++ src/partials/Pages/StatisticsPage/Statistics.jsx
@@ -2,19 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCategories, getStatistics } from '../../../redux/statistics/statisticsOperations';
 import { selectCategories, selectStatisticsData } from '../../../redux/statistics/statisticsSelectors';
-import { DataTable } from '../../Components/Statistics/statisticsDataTable';
+import DataTable from '../../Components/Statistics/statisticsDataTable';
 import DatePicker from '../../Components/Statistics/datePicker';
 import StatisticsChart from '../../Components/Statistics/statisticsChart';
-
+import { PieChart } from 'react-minimal-pie-chart';
+import { css } from '@emotion/react';
 import {
+  PageContainer,
   StatSection,
+  LeftSideWrapper,
   RightSideStatWrapper,
   DatePickerWrapper,
-  DataWrapper,
   TitleStyled,
-  LeftSideWrapper,
   NoTransactionsMessage,
+  ExpenseIncomeWrapper,
+  ExpenseIncomeItem,
+  ExpenseIncomeLabel,
+  ExpenseIncomeValue,
+  CategorySumWrapper,
+  CategorySumItem,
+  DataTableWrapper,
+  ChartWrapper,
+  ChartTitle,
+  PieChartContainer,
 } from './Statistics.styled';
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const StatisticsPage = () => {
   const dispatch = useDispatch();
@@ -63,21 +80,10 @@ const StatisticsPage = () => {
   }, []);
 
   return (
-    <StatSection>
-      <RightSideStatWrapper>
+    <PageContainer>
+      <StatSection>
         <LeftSideWrapper>
           <TitleStyled>Statistics</TitleStyled>
-          {categories && reduxData && !loading ? (
-            reduxData.categoryExpenses.length > 0 ? (
-              <StatisticsChart reduxData={reduxData} categories={categories} />
-            ) : (
-              <NoTransactionsMessage>
-                You don't have any transactions in this period.
-              </NoTransactionsMessage>
-            )
-          ) : null}
-        </LeftSideWrapper>
-        <DataWrapper>
           <DatePickerWrapper>
             <DatePicker
               getYear={handleYearChange}
@@ -86,18 +92,64 @@ const StatisticsPage = () => {
               selectedYear={selectedYear}
             />
           </DatePickerWrapper>
-          {categories && reduxData && !loading ? (
+          <CategorySumWrapper>
+            <CategorySumItem>Category</CategorySumItem>
+            <CategorySumItem>Sum</CategorySumItem>
+          </CategorySumWrapper>
+          <ExpenseIncomeWrapper>
+            <ExpenseIncomeItem>
+              <ExpenseIncomeLabel>Expenses:</ExpenseIncomeLabel>
+              <ExpenseIncomeValue>Ron {reduxData ? reduxData.totalExpenses.toFixed(2) : '0.00'}</ExpenseIncomeValue>
+            </ExpenseIncomeItem>
+            <ExpenseIncomeItem>
+              <ExpenseIncomeLabel>Income:</ExpenseIncomeLabel>
+              <ExpenseIncomeValue>Ron {reduxData ? reduxData.totalIncome.toFixed(2) : '0.00'}</ExpenseIncomeValue>
+            </ExpenseIncomeItem>
+          </ExpenseIncomeWrapper>
+        </LeftSideWrapper>
+        <RightSideStatWrapper>
+          {categories && reduxData ? (
             reduxData.categoryExpenses.length > 0 ? (
-              <DataTable reduxData={reduxData} />
+              <>
+                <ChartWrapper>
+                  <ChartTitle>Statistics Chart</ChartTitle>
+                  <StatisticsChart reduxData={reduxData} categories={categories} />
+                </ChartWrapper>
+                <DataTableWrapper>
+                  <ChartTitle>Statistics Table</ChartTitle>
+                  <DataTable reduxData={reduxData} />
+                </DataTableWrapper>
+              </>
             ) : (
               <NoTransactionsMessage>
                 You don't have any transactions in this period.
               </NoTransactionsMessage>
             )
           ) : null}
-        </DataWrapper>
-      </RightSideStatWrapper>
-    </StatSection>
+          
+          <ChartWrapper>
+            <PieChartContainer>
+              <PieChart
+                data={[
+                  { title: 'Ron', value: 1, color: 'rgba(128, 128, 128, 0.3)' },
+                ]}
+                lineWidth={15}
+                animate
+                animationDuration={900}
+                animationEasing="ease-out"
+                label={({ dataEntry }) => dataEntry.title}
+                labelStyle={{
+                  fontSize: '25px',
+                  fontFamily: 'sans-serif',
+                  fill: '#ffffff', 
+                }}
+                labelPosition={0}
+              />
+            </PieChartContainer>
+          </ChartWrapper>
+        </RightSideStatWrapper>
+      </StatSection>
+    </PageContainer>
   );
 };
 
